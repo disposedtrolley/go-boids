f5b41edfbcb126f27afb0119a6570af8728dea91
jamesliu <jamesliu@au1.ibm.com>
2018-11-21 16:14:35 +1100

    feat: add blue background, centre window on start to circumvent Mojave rendering issue

--- go.mod
+++ go.mod
@@ -8,5 +8,5 @@ require (
 	github.com/go-gl/glfw v0.0.0-20181014061658-691ee1b84c51 // indirect
 	github.com/go-gl/mathgl v0.0.0-20180804195959-cdf14b6b8f8a // indirect
 	github.com/pkg/errors v0.8.0 // indirect
-	golang.org/x/image v0.0.0-20181116024801-cd38e8056d9b // indirect
+	golang.org/x/image v0.0.0-20181116024801-cd38e8056d9b
 )
--- main.go
+++ main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
+
+func centerWindow(win *pixelgl.Window) {
+	x, y := pixelgl.PrimaryMonitor().Size()
+	width, height := win.Bounds().Size().XY()
+	win.SetPos(
+		pixel.V(
+			x/2-width/2,
+			y/2-height/2,
+		),
+	)
+}
 
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "go-boids",
 		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
 	}
 
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
+
+	centerWindow(win)
+
+	win.Clear(colornames.Skyblue)
 
 	for !win.Closed() {
 		win.Update()
